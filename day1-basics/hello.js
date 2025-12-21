8c612aa1b4273099e6ae40369d0e583105789e38
Ted Huang <ted.huang@dowjones.com>
2025-12-21 10:56:56 -0600

    removed deleted files

--- day1-basics/hello.js
+++
@@ -1,5 +0,0 @@
-console.log("Hello, World!");
-console.log("Current Directory:", process.cwd());
-console.log("Node Version:", process.version);
-console.log("Platform:", process.platform);
-console.log("Arguments:", process.argv);
--- day1-basics/src/models/account-cell.ts
+++
@@ -1,9 +0,0 @@
-export class AccountCell {
-    public month: string;
-    public amount: number;
-
-    constructor(month: string, amount: number) {
-        this.month = month;
-        this.amount = amount;
-    }
-}
